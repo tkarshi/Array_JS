f811965d06463b7d99ef1b3546c319cba0241549
tkarshi <tkarshi2000@gmail.com>
2024-07-13 14:26:12 +0530

    for loop display

---
+++ array_loop.js
@@ -0,0 +1,17 @@
+let Students = [
+  {
+    id: 1,
+    name: "Stu1",
+    mobile: [12345, 67890],
+  },
+  {
+    id: 2,
+    name: "Stu2",
+    mobile: [67890, 12345],
+  },
+];
+
+for (i = 0; i < Students.length; i++) {
+  let students = Students[i];
+  console.log(students);
+}

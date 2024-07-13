a650de3ffc1ecd51d771de9466f174872708c596
tkarshi <tkarshi2000@gmail.com>
2024-07-13 09:45:37 +0530

    array

---
+++ index.js
@@ -0,0 +1,46 @@
+// let listOfNumbers = [2, 3, 10, 5, 8];
+
+//All data list will display
+// console.log(listOfNumbers);
+
+//The 1 index position data will display
+// console.log(listOfNumbers[1]);
+
+//The 0 index position data will display
+// console.log(listOfNumbers[0]);
+
+//The 1 index position element will display because (3 - 2 = 1)
+// console.log(listOfNumbers[3 - 2]);
+
+//Count the elements of the array
+// console.log(listOfNumbers.length); //output: 5
+
+//------------------------------------------------------------------------------//
+
+// let listOfStudents = ["Student 1", "Student 2", "Student 3", "Student 4"];
+
+// console.log(listOfStudents[0].toLowerCase()); //output: student 1
+
+// console.log(listOfStudents[1].toUpperCase()); //output: STUDENT 2
+
+//------------------------------------------------------------------------------//
+
+let listNumbers = [2, 8, 7, 50, 25, 100];
+
+// console.log(listNumbers);
+
+// listNumbers.push(2500);
+
+// console.log(listNumbers);
+
+console.log(listNumbers); //output: 2, 8, 7, 50, 25, 100
+
+console.log(listNumbers.pop()); //100 pop
+
+console.log(listNumbers.pop()); //25 pop
+
+listNumbers.push(20);
+
+listNumbers.push(30);
+
+console.log(listNumbers);
